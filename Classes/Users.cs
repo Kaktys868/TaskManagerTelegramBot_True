d49cdc38c823a7c29d300c0526c38099f349310c
Kaktys868 <lastochka1230@yandex.ru>
2025-12-18 21:36:43 +0500

    Созданный классы для приложения. True

---
+++ Classes/Users.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagerTelegramBot_True.Classes
+{
+    public class Users
+    {
+        public long IdUser { get; set; }
+        public List<Events> Events { get; set; }
+        public Users(long idUser)
+        {
+            IdUser = idUser;
+            Events = new List<Events>();
+        }
+    }
+}
